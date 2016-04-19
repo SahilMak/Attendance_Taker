5eafee0635f07a9214990ae96681d90a6c496fe1
Sahil Makhijani <svmakhijani94@gmail.com>
2016-04-18 23:38:04 -0400

    Create Attendance.java

---
+++ Attendance.java
@@ -0,0 +1,8 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
+public class Attendance extends JFrame implements ActionListener {
+}
