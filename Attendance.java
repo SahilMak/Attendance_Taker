3ef0a9447a049afae46e47b01867be1745f5a7ad
Sahil Makhijani <svmakhijani94@gmail.com>
2016-04-18 23:47:27 -0400

    Added imports

--- Attendance.java
+++ Attendance.java
@@ -3,6 +3,9 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 
 public class Attendance extends JFrame implements ActionListener {
   JTextArea list = new JTextArea();
@@ -19,10 +22,10 @@ public class Attendance extends JFrame implements ActionListener {
   String action = "";
   
   public static void main(String[] args) {
-  	JFrame frame = new BrownBoss();
-  	frame.setSize(800, 600);
-  	frame.setResizable(false);
-  	frame.setVisible(true);
-  	frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    JFrame frame = new BrownBoss();
+    frame.setSize(800, 600);
+    frame.setResizable(false);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
   }
 }
