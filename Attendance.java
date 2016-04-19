90adb76d3526b2db6321c4b712589cde3c1f7276
Sahil Makhijani <svmakhijani94@gmail.com>
2016-04-19 19:06:49 -0400

    Created window panel

--- Attendance.java
+++ Attendance.java
@@ -28,4 +28,26 @@ public class Attendance extends JFrame implements ActionListener {
     frame.setVisible(true);
     frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
   }
+  
+  /**
+   * Constructor
+   */
+  
+  public Attendance() {
+    // Set window title
+    setTitle("Attendance");
+    
+    // Specify layout
+    panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+    
+    // Add components
+    panel.add(list);
+    panel.add(add);
+    panel.add(done);
+    add(panel);
+    
+    // Add action listeners
+    add.addActionListener(this);
+    done.addActionListener(this);
+  }
 }
