78d3290a05b35c83a5ed5e0b4fab313de50dae73
Sahil Makhijani <svmakhijani94@gmail.com>
2016-04-18 23:44:11 -0400

    Edited layout

--- Attendance.java
+++ Attendance.java
@@ -6,23 +6,23 @@ import java.io.*;
 
 public class Attendance extends JFrame implements ActionListener {
   JTextArea list = new JTextArea();
-	JTextField name;
-	JTextField email;
-	JButton add = new JButton("Add");
-	JButton done = new JButton("Done");
-	JPanel panel = new JPanel();
-	JPanel info = new JPanel();
-	JPanel labels = new JPanel();
-	JPanel fields;
-	PrintWriter output;
-	String date;
-	String action = "";
-	
-	public static void main(String[] args) {
-		JFrame frame = new BrownBoss();
-		frame.setSize(800, 600);
-		frame.setResizable(false);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+  JTextField name;
+  JTextField email;
+  JButton add = new JButton("Add");
+  JButton done = new JButton("Done");
+  JPanel panel = new JPanel();
+  JPanel info = new JPanel();
+  JPanel labels = new JPanel();
+  JPanel fields;
+  PrintWriter output;
+  String date;
+  String action = "";
+  
+  public static void main(String[] args) {
+  	JFrame frame = new BrownBoss();
+  	frame.setSize(800, 600);
+  	frame.setResizable(false);
+  	frame.setVisible(true);
+  	frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
 }
